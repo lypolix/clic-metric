ada416ca82d14cfdd6887819ecea15f262df4a56
Polina <plusik-006@mail.ru>
2025-08-18 11:46:58 +0300

    Initial commit: clic-metric

---
+++ cmd/clic-metric/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"clic-metric/internal/config"
+	"clic-metric/internal/http-server/handlers/redirect"
+	"clic-metric/internal/http-server/handlers/url/save"
+	"clic-metric/internal/http-server/middleware/logger"
+	"clic-metric/internal/lib/logger/handlers/slogpretty"
+	"clic-metric/internal/lib/logger/sl"
+	"clic-metric/internal/storage/postgres"
+	"log/slog"
+	"net/http"
+	"os"
+	
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
+
+func main() {
+	
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting clic-metric", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+	
+	storage, err := postgres.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage 
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(logger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Route("/url", func(r chi.Router){
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password, 
+		}))
+
+		router.Post("/", save.New(log, storage))
+	})
+
+	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
+	
+	log.Info("starting server", slog.String("addres", cfg.Address))
+
+	srv := &http.Server{
+		Addr: cfg.Address,
+		Handler: router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env{
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}), 
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}), 
+		)
+	}
+
+	return log
+
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,84 @@
+package postgres
+
+import (
+	"clic-metric/internal/storage"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.postgres.New"
+
+	db, err := sql.Open("postgres", storagePath)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = db.Exec(`
+			CREATE TABLE IF NOT EXISTS url(
+				id SERIAL PRIMARY KEY, 
+				alias TEXT NOT NULL UNIQUE, 
+				url TEXT NOT NULL
+        	)	
+    	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = db.Exec(`
+        	CREATE INDEX IF NOT EXISTS index_alias ON url(alias)
+    	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.postgres.SaveURL"
+	var id int64
+
+	err := s.db.QueryRow(
+		"INSERT INTO url(alias, url) VALUES ($1, $2) RETURNING id", alias, urlToSave,
+	).Scan(&id)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUrlExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.postgres.GetUTL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+
+	if err != nil {
+		return "", fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	var resURL string
+	err = stmt.QueryRow(alias).Scan(&resURL)
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", storage.ErrUrlNotFound
+	}
+	if err != nil {
+		return "", fmt.Errorf("%s: execute statement: %w", op, err)
+	}
+
+	return resURL, nil
+
+}
